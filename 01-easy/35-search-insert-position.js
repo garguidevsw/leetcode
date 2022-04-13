10d5b9540e81db94896b844757d5b623e083a991
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2022-04-13 16:00:47 -0500

    Se agrega ejercicio 35

---
+++ 01-easy/35-search-insert-position.js
@@ -0,0 +1,28 @@
+
+// @param {number[]} nums
+// @param {number} target
+// @return {number}
+
+const searchInsert = (nums, target) => {
+    let idx = nums.indexOf(target);
+
+    if(idx != -1){
+        return idx;
+    }
+
+    idx = 0;
+    nums.forEach(element => {
+        if(element < target){
+            idx++;
+        }
+    });
+
+    return idx;
+
+    
+};
+console.log(searchInsert([1,3,5,6], 5));
+console.log(searchInsert([1,3,5,6], 2));
+console.log(searchInsert([1,3,5,6], 7));
+console.log(searchInsert([1,3,5,6], 0));
+
